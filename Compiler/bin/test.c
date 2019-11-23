4d2a983f5dce4e3c90c5acea233a7f67bdb49290
YouJuwon <edgecoding@naver.com>
2019-11-23 20:30:16 +0900

    Init project

---
+++ Compiler/bin/test.c
@@ -0,0 +1,10 @@
+nt add(int x, int y) {
+	int z ;
+	z = x+y;
+	return z;
+}
+
+void main () {
+	int t = 33;  
+	_print(add(1,t));
+}
---
+++ Compiler/src/listener/main/BytecodeGenListenerHelper.java
@@ -0,0 +1,126 @@
+package listener.main;
+
+import listener.main.MiniCParser.ExprContext;
+import listener.main.MiniCParser.Fun_declContext;
+import listener.main.MiniCParser.If_stmtContext;
+import listener.main.MiniCParser.Local_declContext;
+import listener.main.MiniCParser.ParamContext;
+import listener.main.MiniCParser.ParamsContext;
+import listener.main.MiniCParser.Type_specContext;
+import listener.main.MiniCParser.Var_declContext;
+
+public class BytecodeGenListenerHelper {
+	
+	// <boolean functions>
+	
+	static boolean isFunDecl(MiniCParser.ProgramContext ctx, int i) {
+		return ctx.getChild(i).getChild(0) instanceof MiniCParser.Fun_declContext;
+	}
+	
+	// type_spec IDENT '[' ']'
+	static boolean isArrayParamDecl(ParamContext param) {
+		return param.getChildCount() == 4;
+	}
+	
+	// global vars
+	static int initVal(Var_declContext ctx) {
+		return Integer.parseInt(ctx.LITERAL().getText());
+	}
+
+	// var_decl	: type_spec IDENT '=' LITERAL ';
+	static boolean isDeclWithInit(Var_declContext ctx) {
+		return ctx.getChildCount() == 5 ;
+	}
+	// var_decl	: type_spec IDENT '[' LITERAL ']' ';'
+	static boolean isArrayDecl(Var_declContext ctx) {
+		return ctx.getChildCount() == 6;
+	}
+
+	// <local vars>
+	// local_decl	: type_spec IDENT '[' LITERAL ']' ';'
+	static int initVal(Local_declContext ctx) {
+		return Integer.parseInt(ctx.LITERAL().getText());
+	}
+
+	static boolean isArrayDecl(Local_declContext ctx) {
+		return ctx.getChildCount() == 6;
+	}
+	
+	static boolean isDeclWithInit(Local_declContext ctx) {
+		return ctx.getChildCount() == 5 ;
+	}
+	
+	static boolean isVoidF(Fun_declContext ctx) {
+			// <Fill in>
+		return false; //have to modify
+	}
+	
+	static boolean isIntReturn(MiniCParser.Return_stmtContext ctx) {
+		return ctx.getChildCount() ==3;
+	}
+
+
+	static boolean isVoidReturn(MiniCParser.Return_stmtContext ctx) {
+		return ctx.getChildCount() == 2;
+	}
+	
+	// <information extraction>
+	static String getStackSize(Fun_declContext ctx) {
+		return "32";
+	}
+	static String getLocalVarSize(Fun_declContext ctx) {
+		return "32";
+	}
+	static String getTypeText(Type_specContext typespec) {
+			// <Fill in>
+		return null;	//have to modify
+	}
+
+	// params
+	static String getParamName(ParamContext param) {
+		// <Fill in>
+		return null; //have to modify
+	}
+	
+	static String getParamTypesText(ParamsContext params) {
+		String typeText = "";
+		
+		for(int i = 0; i < params.param().size(); i++) {
+			MiniCParser.Type_specContext typespec = (MiniCParser.Type_specContext)  params.param(i).getChild(0);
+			typeText += getTypeText(typespec); // + ";";
+		}
+		return typeText;
+	}
+	
+	static String getLocalVarName(Local_declContext local_decl) {
+		// <Fill in>
+		return null; //have to modify
+	}
+	
+	static String getFunName(Fun_declContext ctx) {
+		// <Fill in>
+		return null; //have to modify
+	}
+	
+	static String getFunName(ExprContext ctx) {
+		// <Fill in>
+		return null; //have to modify
+	}
+	
+	static boolean noElse(If_stmtContext ctx) {
+		return ctx.getChildCount() < 5;
+	}
+	
+	static String getFunProlog() {
+		// return ".class public Test .....
+		// ...
+		// invokenonvirtual java/lang/Object/<init>()
+		// return
+		// .end method"
+		return null; //have to modify
+	}
+	
+	static String getCurrentClassName() {
+		return "Test";
+	}
+}
